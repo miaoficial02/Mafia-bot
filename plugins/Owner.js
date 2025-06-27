c4d1512c7e6b9d5baf4ca6418a0f491392a1ed08
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-06-27 02:41:40 -0300

    Update Owner.js

--- plugins/Owner.js
+++ plugins/Owner.js
@@ -2,13 +2,11 @@ const handler = async (m, { conn }) => {
   const ownerNumber = '5492644131963';
   const ownerName = 'Mía';
   const github = 'https://github.com/miaoficial02';
-
-  await conn.sendContact(m.chat, [[ownerNumber, `${ownerName} - Dueña del Bot`]], m);
-
+  
   const info = `
 ╭━━〔 👑 *OWNER* 👑 〕━━⬣
 ┃ ✨ *Nombre:* ${ownerName}
-┃ 📱 *Edad:* 18
+┃ 📱 *Número:* wa.me/${ownerNumber}
 ┃ 💻 *GitHub:* ${github}
 ╰━━━━━━━━━━━━━━━━━━━━⬣
 `.trim();
